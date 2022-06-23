fa005974bfeb4bd6e2e4d1b73aa6df5a35aeac84
Sanjay <sanjay.bhatt371@webkul.com>
2022-06-23 17:33:35 +0530

    Cutomer-Login-And-AddToCart-Implemented

--- bagistoMaven/src/main/java/com/bagistoCore/PlaceOrderAutomate.java
+++ bagistoMaven/src/main/java/com/bagistoCore/PlaceOrderAutomate.java
@@ -2,6 +2,7 @@ package com.bagistoCore;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -18,22 +19,26 @@ public class PlaceOrderAutomate {
 		
 		driver.manage().window().maximize(); //Mazimize current window
 
-		Thread.sleep(1000);    //Delay execution for 5 seconds to view the maximize operation
+		Thread.sleep(1000);    //Delay execution for 5 seconds to view the maximize operation		
+	
+		driver.findElement(By.className("welcome-content")).click();
+		Thread.sleep(1000); 
+		driver.findElement(By.id("login")).click(); 
+		Thread.sleep(1000); 
+		driver.findElement(By.name("email")).sendKeys("sanjay.bhatt371@webkul.com");  
+		driver.findElement(By.name("password")).sendKeys("Sanjay@123#@!"); 
+		Thread.sleep(1000); 
+		System.out.println("sign success");			
+		driver.findElement(By.id("userLogin")).click();
+		Thread.sleep(1000); 
+		driver.findElement(By.className("logo")).click();		
+		Thread.sleep(3000); 
+		driver.findElement(By.id("Red Wayfarer Sunglasses")).click(); 		
+		Thread.sleep(2000);		
+		driver.findElement(By.className("rango-plus")).click();	
+		Thread.sleep(2000); 
+		driver.findElement(By.className("add-to-cart-btn")).click();
 		
-		driver.findElement(By.id("Red Wayfarer Sunglasses")).click(); 
-		
-		Thread.sleep(1000);
-		
-		driver.findElement(By.className("rango-plus")).click();
-		//driver.findElement(By.xpath("<button>[text()=’Buy Now’]")).click();
-		
-		
-		/*driver.findElement(By.id("email")).sendKeys("admin@example.com");  
-		driver.findElement(By.id("password")).sendKeys("admin123"); 
-		
-		driver.findElement(By.className("btn-primary")).click();  
-		
-		driver.quit(); */
 
 	}
 
