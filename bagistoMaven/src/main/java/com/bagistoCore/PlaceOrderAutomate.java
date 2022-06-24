fd881b52fdc27dd9a610c6b712cf32b4529a0a1a
Sanjay <sanjay.bhatt371@webkul.com>
2022-06-24 16:12:04 +0530

    Admin-Login-in-NewTab

--- bagistoMaven/src/main/java/com/bagistoCore/PlaceOrderAutomate.java
+++ bagistoMaven/src/main/java/com/bagistoCore/PlaceOrderAutomate.java
@@ -3,6 +3,7 @@ package com.bagistoCore;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -19,34 +20,43 @@ public class PlaceOrderAutomate {
 		
 		driver.manage().window().maximize(); //Mazimize current window
 
-		Thread.sleep(1000);    //Delay execution for 5 seconds to view the maximize operation		
-	
+		Thread.sleep(2000);    //Delay execution for 5 seconds to view the maximize operation				
+		
 		driver.findElement(By.className("welcome-content")).click();
 		Thread.sleep(1000); 
 		driver.findElement(By.id("login")).click(); 
 		Thread.sleep(1000); 
 		driver.findElement(By.name("email")).sendKeys("sanjay.bhatt371@webkul.com");  
 		driver.findElement(By.name("password")).sendKeys("Sanjay@123#@!"); 
-		Thread.sleep(1000); 
-		System.out.println("sign success");			
+		Thread.sleep(1000); 		
 		driver.findElement(By.id("userLogin")).click();
+		
+		System.out.println("sign success");	
+		
 		Thread.sleep(1000); 
 		driver.findElement(By.className("logo")).click();		
-		Thread.sleep(3000); 
+		Thread.sleep(9000); 
 		driver.findElement(By.id("Red Wayfarer Sunglasses")).click(); 		
 		Thread.sleep(2000);		
 		driver.findElement(By.className("rango-plus")).click();	
 		Thread.sleep(2000); 
 		driver.findElement(By.className("add-to-cart-btn")).click();
+		
 		System.out.println("Add to cart successfully");
-		Thread.sleep(7000); 
+		
+		Thread.sleep(8000);
+		driver.findElement(By.id("mini-cart")).click();
+		
 		driver.findElement(By.className("material-icons-outlined")).click();
-		Thread.sleep(2000); 
+		
+		Thread.sleep(3000); 
 		driver.findElement(By.className("remove-decoration")).click();
 		Thread.sleep(4000); 
 		driver.findElement(By.className("text-uppercase")).click();
 		Thread.sleep(8000);
 		driver.findElement(By.cssSelector("input[type='radio']")).click();
+		
+		System.out.println("Address saved successfully");
 		Thread.sleep(8000);
 		driver.findElement(By.id("free_free")).click();
 		Thread.sleep(9000);
@@ -54,7 +64,21 @@ public class PlaceOrderAutomate {
 		Thread.sleep(9000);
 		driver.findElement(By.id("checkout-place-order-button")).click();
 		System.out.println("Order Created successfully");
-		driver.quit(); 
+		
+		// OPen Admin Panel In New Tab		
+		driver.switchTo().newWindow(WindowType.TAB);		
+		driver.navigate().to("http://192.168.15.237/sanjay-bagisot/public/backend/login");
+		
+		System.out.println("Admin Panel Opened In New Tab SuccessFully");
+		
+		driver.findElement(By.id("email")).sendKeys("admin@example.com");  
+		driver.findElement(By.id("password")).sendKeys("admin123"); 
+		System.out.println("Text Field Cleared");	
+		driver.findElement(By.className("btn-primary")).click(); 
+		
+		System.out.println("Admin Login successfully");		
+		
+		//driver.quit(); 
 
 	}
 
