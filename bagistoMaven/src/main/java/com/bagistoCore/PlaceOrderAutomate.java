be6061045aaed71d0c01ebbac7a62f9e22f433ed
sanjay bhatt <sanjay.bhatt371@webkul.com>
2022-07-05 15:50:53 +0530

    Update-placeOrder-Script

--- bagistoMaven/src/main/java/com/bagistoCore/PlaceOrderAutomate.java
+++ bagistoMaven/src/main/java/com/bagistoCore/PlaceOrderAutomate.java
@@ -19,7 +19,7 @@ public class PlaceOrderAutomate {
 		WebDriver driver = new ChromeDriver(); // create object of interface
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
-		driver.get("http://192.168.15.237/sanjay-bagisot/public/");		
+		driver.get("http://192.168.15.237/sanjay-bagisto/public/");		
 		
 		driver.manage().window().maximize(); //Mazimize current window
 
@@ -28,19 +28,19 @@ public class PlaceOrderAutomate {
 		driver.findElement(By.className("welcome-content")).click();
 		
 		
-		driver.findElement(By.id("login")).click(); 
-		
-		driver.findElement(By.name("email")).sendKeys("sanjay.bhatt371@webkul.com");  
+		driver.findElement(By.linkText("Sign In")).click();
+		//driver.findElement(By.id("login")).click(); 
+		Thread.sleep(1000);
+		driver.findElement(By.name("email")).sendKeys("sanjay.bhatt371@webkul.in");  
 		driver.findElement(By.name("password")).sendKeys("Sanjay@123#@!"); 
-		Thread.sleep(1000); 
+		driver.findElement(By.cssSelector("input[type='submit']")).click();
 		System.out.println("Customer Login succesfully");
+		driver.findElement(By.className("logo")).click();
 		
-		driver.findElement(By.id("userLogin")).click();		
-		System.out.println("sign success");	
+		driver.findElement(By.xpath("//img[@alt='Sunglasses']")).click();
+
+		//driver.findElement(By.xpath("//img[contains(@alt,'Sunglasses')]")).click(); 
 		
-		
-		driver.findElement(By.className("logo")).click();		
-		driver.findElement(By.id("Red Wayfarer Sunglasses")).click(); 		
 		driver.findElement(By.className("rango-plus")).click();	
 		Thread.sleep(2000); 
 		driver.findElement(By.className("add-to-cart-btn")).click();
@@ -62,50 +62,47 @@ public class PlaceOrderAutomate {
 		
 		//Thread.sleep(3000);  // click on saved address
 		driver.findElement(By.cssSelector("input[type='radio']")).click();
+		System.out.println("Address saved successfully");
+		driver.findElement(By.cssSelector("form li")).click();
+		
+		Thread.sleep(2000);  // shipping address selected			
+		driver.findElement(By.xpath("//input[@id='free_free']")).click();
 
-		System.out.println("Address saved successfully");	
-		
-		// Scroll page down 
-		JavascriptExecutor js = (JavascriptExecutor) driver;		
-	
-		String Element = driver.findElement(By.xpath("//div[@class='shipping-methods']/b[1]")).getText();
-
-		 System.out.println(Element);
-        // Scrolling down the page till the element is found		
-        js.executeScript("arguments[0].scrollIntoView();", Element);
-        
-				 
-		Thread.sleep(4000);  // shipping address selected		
-		driver.findElement(By.cssSelector("#free_free")).click();
+		System.out.println("Shipping method selected successfully");	
 		
 		
-		System.out.println("Shipping method selected successfully");
-		
-		// Scroll page down 
-		JavascriptExecutor js_one = (JavascriptExecutor) driver;
-		js_one.executeScript("window.scrollBy(0,-350)", "");
-		
-		
-		Thread.sleep(9000); // payment methos selected			
+		Thread.sleep(1000); // payment methos selected			
 		driver.findElement(By.id("cashondelivery")).click();		
 		System.out.println("Payment method selected successfully");
 		
-		Thread.sleep(9000); // place order button clicked
+		Thread.sleep(2000); // place order button clicked
 		driver.findElement(By.id("checkout-place-order-button")).click();
 		System.out.println("Order Created successfully");
+		Thread.sleep(3000);
+		System.out.println(driver.findElement(By.xpath("//div[@class='order-success-content']/a[1]")).getText());
+		
 		
 		// OPen Admin Panel In New Tab		
 		driver.switchTo().newWindow(WindowType.TAB);		
-		driver.navigate().to("http://192.168.15.237/sanjay-bagisot/public/backend/login");
+		driver.navigate().to("http://192.168.15.237/sanjay-bagisto/public/admin/login");
 		
 		System.out.println("Admin Panel Opened In New Tab SuccessFully");
 		
 		driver.findElement(By.id("email")).sendKeys("admin@example.com");  
-		driver.findElement(By.id("password")).sendKeys("admin123"); 
-		System.out.println("Text Field Cleared");	
+		driver.findElement(By.id("password")).sendKeys("admin123"); 	
 		driver.findElement(By.className("btn-primary")).click(); 
+		System.out.println("Admin Login Successfully");
 		
-		System.out.println("Admin Login successfully");		
+		// CREATE SHIPPING
+		driver.findElement(By.xpath("//div[@class='navbar-left']/ul/li[2]")).click();
+		System.out.println("Orders Page Opened");
+		driver.findElement(By.linkText("http://192.168.15.237/sanjay-bagisto/public/admin/sales/orders/view/44")).click();
+		driver.findElement(By.xpath("//div[@class='page-action']/a[3]")).click();
+		System.out.println("Shiiping Button Clicked");
+		driver.findElement(By.tagName("select")).click();
+		driver.findElement(By.xpath("//select[@class='control']/option[2]")).click();
+		driver.findElement(By.cssSelector("form button")).click();		
+		System.out.println("Shipping Created Successfully");
 		
 		//driver.quit(); 
 
