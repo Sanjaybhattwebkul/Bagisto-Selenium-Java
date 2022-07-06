ea11648faae93a6a5b6aa612d0ddf1955f20e259
sanjay bhatt <sanjay.bhatt371@webkul.com>
2022-07-06 19:11:28 +0530

    Pass-URL-as-Argument

--- bagistoMaven/src/main/java/PlaceOrderBysearchProduct.java
+++ bagistoMaven/src/main/java/PlaceOrderBysearchProduct.java
@@ -10,36 +10,40 @@ public class PlaceOrderBysearchProduct {
 		System.setProperty("webdriver.chrome.driver", "/home/users/sanjay.bhatt/chromedriver_linux64/chromedriver");
 		WebDriver driver = new ChromeDriver(); 
 		
+		String firstName = "Bagisto";
+		String lastName = "Admin";
+		String email  = "admin@bagisto.com";
+		String password = "admin123";
+		String URL = "http://192.168.15.237/sanjay-bagisto/public/";
+		
 		// GetURL
-		getUrl(driver,"http://192.168.15.237/sanjay-bagisto/public/");
+		getUrl(driver,URL);
 	
 		// SEARCH PRODUCT
-		searchProduct(driver,"cycle");
+		searchProduct(driver,"cycle",URL);
 		
 		// ADD TO COMPARE LIST
 		driver.findElement(By.xpath("//a[@title='Add product to compare list']")).click();	
 		System.out.println("Product Added To compare successfully");
 		driver.findElement(By.xpath("//a[text()='×']")).click();
-		driver.findElement(By.cssSelector("a[href*='http://192.168.15.237/sanjay-bagisto/public/comparison']")).click();
+		driver.findElement(By.cssSelector("a[href*='"+URL+"comparison']")).click();
 		addToWishlist(driver,"//a[@title='Add product to wishlist']");				
 		
-		// CUSTOMER SIGNUP
-		String email  = "admin@bagisto.com";
-		String password = "admin123";	
-		customerSignup(driver,"Bagisto","Admin",email,password);		
+		// CUSTOMER SIGNUP	
+		customerSignup(driver,firstName,lastName,email,password,URL);		
 		
 		// CUSTOMER LOGIN		
 		Thread.sleep(1000);
 		customerLogin(driver,email,password);		
 		
 		//Add Product To Wishlist after search product
-		searchProduct(driver,"cycle");
+		searchProduct(driver,"cycle",URL);
 		addToWishlist(driver,"//a[@title='Add product to wishlist']");
 	}
 	
-	public static void customerSignup(WebDriver driver,String firstName, String lastName, String email, String password)
+	public static void customerSignup(WebDriver driver,String firstName, String lastName, String email, String password,String URL)
 	{
-		driver.get("http://192.168.15.237/sanjay-bagisto/public/customer/register");
+		driver.get(URL+"customer/register");
 		driver.findElement(By.name("first_name")).sendKeys(firstName);
 		driver.findElement(By.name("last_name")).sendKeys(lastName);
 		driver.findElement(By.name("email")).sendKeys(email);
@@ -58,9 +62,9 @@ public class PlaceOrderBysearchProduct {
 		System.out.println("Customer Login Successfull");
 		
 	}
-	public static void searchProduct(WebDriver driver, String searchText)
+	public static void searchProduct(WebDriver driver, String searchText,String URL)
 	{
-		driver.get("http://192.168.15.237/sanjay-bagisto/public/");			
+		driver.get(URL);			
 		driver.findElement(By.cssSelector("input[placeholder='Search products here']")).sendKeys(searchText);
 		driver.findElement(By.className("rango-search")).click();
 		driver.findElement(By.xpath("//img[@alt='Cycle on rent']")).click();
